d90645914b070ceaf835e2be92e58a6a764f45e8
LAW <li_aiwu@126.com>
2025-01-14 10:56:30 +0800

    Add ignored directories when backing up.

--- AttnSoft.AutoUpdate/Middlewares/BackupMiddleware.cs
+++ AttnSoft.AutoUpdate/Middlewares/BackupMiddleware.cs
@@ -15,7 +15,11 @@ public class BackupMiddleware : IBackup
     public async Task InvokeAsync(ApplicationDelegate<UpdateContext> next, UpdateContext context)
     {
         context.BackupPath = Path.Combine(context.AppPath,
-            $"{StorageManager.DirectoryName}{context.GetClientVersion()}");
+            $"{StorageManager.DirectoryName}{context.ClientVersion}");
+
+        BlackListManager.Instance.AddSkipDirectorys(context.UpdateVersion.SkipDirectorys);
+        //BlackListManager.Instance.AddBlackFileFormats(context.UpdateVersion.BlackFormats);
+        //BlackListManager.Instance.AddBlackFiles(context.UpdateVersion.BlackFiles);
         StorageManager.Backup(context.AppPath, context.BackupPath, BlackListManager.Instance.SkipDirectorys);
 
         await next(context);
