a7f36de43f7a39a879baf8bcd4dd3b269520887d
LAW <li_aiwu@126.com>
2025-01-14 22:34:32 +0800

    更新示例改为手动更新

--- Examples/WinAppClient/Form1.Designer.cs
+++ Examples/WinAppClient/Form1.Designer.cs
@@ -30,60 +30,75 @@
         {
             button1 = new Button();
             panel1 = new Panel();
-            richTextBox1 = new RichTextBox();
-            button2 = new Button();
+            label2 = new Label();
+            label1 = new Label();
+            label3 = new Label();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(64, 29);
+            button1.Font = new Font("Microsoft YaHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(272, 244);
             button1.Name = "button1";
-            button1.Size = new Size(150, 60);
+            button1.Size = new Size(305, 85);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "检查新版本";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
             // panel1
             // 
-            panel1.Controls.Add(button2);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
             panel1.Controls.Add(button1);
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1233, 118);
+            panel1.Size = new Size(856, 516);
             panel1.TabIndex = 1;
             // 
-            // richTextBox1
+            // label2
             // 
-            richTextBox1.Dock = DockStyle.Fill;
-            richTextBox1.Location = new Point(0, 118);
-            richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(1233, 712);
-            richTextBox1.TabIndex = 2;
-            richTextBox1.Text = "";
+            label2.AutoSize = true;
+            label2.Location = new Point(308, 355);
+            label2.Name = "label2";
+            label2.Size = new Size(222, 31);
+            label2.TabIndex = 2;
+            label2.Text = "(为了演示手动更新)";
             // 
-            // button2
+            // label1
             // 
-            button2.Location = new Point(247, 29);
-            button2.Name = "button2";
-            button2.Size = new Size(150, 60);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft YaHei UI", 16.125F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(250, 125);
+            label1.Name = "label1";
+            label1.Size = new Size(327, 57);
+            label1.TabIndex = 1;
+            label1.Text = "我是版本1.0.0.0";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft YaHei UI", 16.125F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(250, 422);
+            label3.Name = "label3";
+            label3.Size = new Size(327, 57);
+            label3.TabIndex = 3;
+            label3.Text = "我是版本1.0.0.0";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(14F, 31F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1233, 830);
-            Controls.Add(richTextBox1);
+            ClientSize = new Size(856, 553);
             Controls.Add(panel1);
             Name = "Form1";
-            Text = "我是版本1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "版本升级测试";
             panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -91,7 +106,8 @@
 
         private Button button1;
         private Panel panel1;
-        private RichTextBox richTextBox1;
-        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
--- Examples/WinAppClient/Form1.cs
+++ Examples/WinAppClient/Form1.cs
@@ -1,7 +1,5 @@
 using AttnSoft.AutoUpdate;
-using AttnSoft.AutoUpdate.Middlewares;
-using GeneralUpdate.Common.HashAlgorithms;
-using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace WinAppClient
 {
@@ -10,86 +8,33 @@ namespace WinAppClient
         public Form1()
         {
             InitializeComponent();
+            this.label1.Text = "升级依据版本 V"+ myUpdateApp.UpContext.ClientVersion.ToString();
+            this.label3.Text = "文件版本 V" + Assembly.GetEntryAssembly().GetName().Version.ToString();
+            myUpdateApp.UpContext.OnFindNewVersion += OnFindNewVersion;
         }
-
+        private async Task OnFindNewVersion(ApplicationDelegate<UpdateContext> next, UpdateContext context)
+        {
+            ShowNewVersion(context);
+            await next(context);
+        }
+        private void ShowNewVersion(UpdateContext context)
+        {
+            if (this.InvokeRequired)
+            {
+                MethodInvoker deleg = delegate { ShowNewVersion(context); };
+                this.BeginInvoke(deleg);
+                return;
+            }
+            MessageBox.Show("发现新版本:"+ context.UpdateVersion.Version.ToString());
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-            //var md5Hash = new Sha256HashAlgorithm();
-            //this.richTextBox1.Text = md5Hash.ComputeHash("WinAppClient.zip");
-            //var verOss = new VersionOSS();
-            //verOss.PubTime = DateTime.Now;
-            //this.richTextBox1.Text = JsonSerializer.Serialize(verOss);
+            myUpdateApp.StartUpdate();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //var md5Hash = new Sha256HashAlgorithm();
-            //this.richTextBox1.Text = md5Hash.ComputeHash("packet_20241125233523804_1.0.0.1.zip");
-        }
-        protected override async void OnLoad(EventArgs e)
-        {
-            base.OnLoad(e);
-            //测量运行时间
-            //var stopwatch = new System.Diagnostics.Stopwatch();
-            //stopwatch.Start();
 
-            //await StartUpdate();
-
-            //stopwatch.Stop();
-            //Console.WriteLine($"StartUpdate函数运行时间：{stopwatch.ElapsedMilliseconds} 毫秒");
-        }
-        private async Task StartUpdate()
-        {
-            try
-            {
-                Console.WriteLine($"主程序初始化，{DateTime.Now}！");
-                Console.WriteLine("当前运行目录：" + Thread.GetDomain().BaseDirectory);
-                await Task.Delay(100);
-
-                var context = new UpdateContext()
-                { 
-                    ClientVersion = new Version("1.0.0.0"),
-                    UpdateUrl = "http://127.0.0.1:5000/Verification" 
-                    //UpdateUrl = "http://localhost/packages/version.json"
-                };
-                context.UseWebApiGetVersionInfo();
-                //context.Services.AddSingleton<ICheckUpdate, CheckUpdateUseWebApiMiddleware>();
-                context.OnUpdateException += OnUpdateException;
-                context.OnDownloadCompleted += OnDownloadCompleted;
-                context.OnDownloadProgressChanged += OnDownloadProgressChanged;
-                //context.OnGetUpdateVersionInfo = (context) =>
-                //{
-                //    return new List<VersionInfo>
-                //    {
-                //        new VersionInfo
-                //        {
-                //            Version = new Version("1.0.0.1"),
-                //        }
-                //    };
-                //};
-
-                await UpdateApp.CreateBuilder(context).StartUpdateAsync() ;
-                //await builder.StartUpdateAsync();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message + "\n" + ex.StackTrace);
-            }
-        }
-
-        private void OnDownloadProgressChanged(long arg1, int arg2)
-        {
-            Console.WriteLine($"当前下载版本：总大小：{arg1}, 进度百分比：{arg2}%");
-        }
-
-        private void OnDownloadCompleted()
-        {
-            Console.WriteLine("升级包下载完成");
-        }
-
-        void OnUpdateException(Exception ex)
-        {
-            Console.WriteLine(ex.Message + "\n" + ex.StackTrace);
         }
 
 
